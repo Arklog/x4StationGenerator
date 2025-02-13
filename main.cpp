3e1cffdce425358322629bb1af2ce6a3eff84fff
Arklog <49959852+Arklog@users.noreply.github.com>
2025-02-13 20:20:07 +0100

    Command line (#2)
    * added qwindows.dll
    
    * added qwindows.dll
    
    * updated action
    
    * updated action
    
    * typo
    
    * Revert "added qwindows.dll"
    
    This reverts commit 45d8f18a
    
    * preserve path structure in archive
    
    * preserve path structure in archive
    
    * preserve path structure in archive
    
    * preserve path structure in archive
    
    * Added command line argument v1
    
    ---------
    
    Co-authored-by: PierreDucloux <pierre@kleep.ai>

--- main.cpp
+++ main.cpp
@@ -1,9 +1,67 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QCommandLineParser>
+#include <QCommandLineOption>
+#include <iostream>
+#include <argp.h>
 #include "mainwindow.h"
+#include "modules.hpp"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QApplication a(argc, argv);
+
+    // Argument parsing
+    QCommandLineParser parser;
+    parser.setApplicationDescription("X4: Foundation complex generator");
+    parser.addHelpOption();
+
+    QCommandLineOption moduleListOption(
+            QStringList() << "m" << "module-list",
+            "Modules to be generated",
+            "Module name"
+    );
+    parser.addOption(moduleListOption);
+
+    QCommandLineOption moduleNumberOption(
+            QStringList() << "n" << "module-number", "Number of modules", "Number"
+    );
+    parser.addOption(moduleNumberOption);
+
+    parser.process(a);
+
+    if (parser.isSet(moduleListOption) != parser.isSet(moduleNumberOption))
+    {
+        std::cerr << "Both module-list and module-number must be set" << std::endl;
+        return 1;
+    } else if (parser.isSet(moduleListOption) && parser.isSet(moduleNumberOption))
+    {
+        t_modules modules;
+        QString   moduleList = parser.value(moduleListOption);
+        size_t    nmodules   = parser.value(moduleNumberOption).toUInt();
+
+        auto module = std::find_if(
+                MODULES::MODULES.cbegin(),
+                MODULES::MODULES.cend(),
+                [&moduleList](const Module &m) {
+                    std::cout << m.name << std::endl;
+                    return m.name == moduleList.toStdString();
+                }
+        );
+
+        if (module == MODULES::MODULES.cend())
+        {
+            std::cerr << "Module not found" << std::endl;
+            return 1;
+        }
+        modules[*module] = nmodules;
+
+        StationBuilder stationBuilder(modules, true);
+        auto           plan = genModulePlan("plan", stationBuilder.get());
+        std::cout << plan << std::endl;
+        return 0;
+    }
+
     MainWindow w;
     w.show();
     return QApplication::exec();
