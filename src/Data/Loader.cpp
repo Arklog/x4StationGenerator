d6e6c96c2a1d955d0b93e0ef52d75962d1711439
PierreDucloux <pierre@kleep.ai>
2025-07-15 21:50:06 +0200

    added all ressources and modules

---
+++ src/Data/Loader.cpp
@@ -0,0 +1,10 @@
+//
+// Created by pierre on 7/15/25.
+//
+
+#include "Loader.h"
+#include "nlohmann/json.hpp"
+
+void Loader::__load_wares() {
+    static const std::string wares_path = "assets/wares";
+}
---
+++ src/Data/Loader.h
@@ -0,0 +1,21 @@
+//
+// Created by pierre on 7/15/25.
+//
+
+#ifndef LOADER_H
+#define LOADER_H
+
+
+
+class Loader {
+private:
+    void __load_wares();
+    void __load_modules();
+
+public:
+    Loader();
+};
+
+
+
+#endif //LOADER_H
