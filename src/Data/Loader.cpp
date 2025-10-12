9171ca201edcc78b76b7dbc0de0b2ea93684439c
Arklog <pierr.ducloux@gmail.com>
2025-10-12 20:42:28 +0200

    Module dir set back

--- src/Data/Loader.cpp
+++ src/Data/Loader.cpp
@@ -12,66 +12,66 @@
 #include "nlohmann/json.hpp"
 #include "spdlog/spdlog.h"
 
-// static const auto module_path = QString("assets/modules");
+static const auto module_path = QString("assets/modules");
 static const auto workforce_path = QString("assets/workforce.json");
-static const auto module_path =
-    QString("tools/regenerate_ware_and_modules/output/modules");
+// static const auto module_path =
+// QString("tools/regenerate_ware_and_modules/output/modules");
 
 void Loader::_loadModules() {
-  spdlog::info("Loading modules");
+    spdlog::info("Loading modules");
 
-  for (const auto &file_iterator :
-       QDirListing(module_path, QDirListing::IteratorFlag::FilesOnly)) {
-    try {
-      spdlog::info("loading: {}", file_iterator.fileName().toStdString());
-      std::fstream file(file_iterator.absoluteFilePath().toStdString(),
-                        std::fstream::in);
-      if (!file.is_open())
-        throw std::runtime_error("Failed to open file " +
-                                 file_iterator.fileName().toStdString());
-      if (file.bad()) {
-        throw std::runtime_error("Failed to read file " +
-                                 file_iterator.fileName().toStdString());
-      }
+    for (const auto &file_iterator:
+         QDirListing(module_path, QDirListing::IteratorFlag::FilesOnly)) {
+        try {
+            spdlog::info("loading: {}", file_iterator.fileName().toStdString());
+            std::fstream file(file_iterator.absoluteFilePath().toStdString(),
+                              std::fstream::in);
+            if (!file.is_open())
+                throw std::runtime_error("Failed to open file " +
+                                         file_iterator.fileName().toStdString());
+            if (file.bad()) {
+                throw std::runtime_error("Failed to read file " +
+                                         file_iterator.fileName().toStdString());
+            }
 
-      auto j = nlohmann::json::parse(file);
-      this->_modules_json.push_back(j.get<Module>());
-      file.close();
-
-    } catch (const std::exception &e) {
-      spdlog::error("could not parse {} : {}",
-                    file_iterator.absoluteFilePath().toStdString(), e.what());
+            auto j = nlohmann::json::parse(file);
+            this->_modules_json.push_back(j.get<Module>());
+            file.close();
+        } catch (const std::exception &e) {
+            spdlog::error("could not parse {} : {}",
+                          file_iterator.absoluteFilePath().toStdString(), e.what());
+        }
     }
-  }
 }
 
 void Loader::_loadWorkforce() {
-  spdlog::info("Loading workforce");
+    spdlog::info("Loading workforce");
 
-  try {
-    std::ifstream file(workforce_path.toStdString(), std::ifstream::in);
-    if (!file.is_open())
-      throw std::runtime_error("Failed to open file " +
-                               workforce_path.toStdString());
-    if (file.bad())
-      throw std::runtime_error("Failed to read file " +
-                               workforce_path.toStdString());
+    try {
+        std::ifstream file(workforce_path.toStdString(), std::ifstream::in);
+        if (!file.is_open())
+            throw std::runtime_error("Failed to open file " +
+                                     workforce_path.toStdString());
+        if (file.bad())
+            throw std::runtime_error("Failed to read file " +
+                                     workforce_path.toStdString());
 
-    auto j = nlohmann::json::parse(file);
-    this->_workforce = j.get<
-        std::map<std::string, std::vector<std::pair<t_ware_id, double>>>>();
-    file.close();
+        auto j = nlohmann::json::parse(file);
+        this->_workforce = j.get<
+            std::map<std::string, std::vector<std::pair<t_ware_id, double> > > >();
+        file.close();
 
-    spdlog::debug("found {} workforce", this->_workforce.size());
-  } catch (const std::exception &e) {
-    spdlog::error("could not parse workforce: {}", e.what());
-    throw;
-  }
+        spdlog::debug("found {} workforce", this->_workforce.size());
+    } catch (const std::exception &e) {
+        spdlog::error("could not parse workforce: {}", e.what());
+        throw;
+    }
 }
 
-void Loader::_parse_wares() {}
+void Loader::_parse_wares() {
+}
 
 void Loader::load() {
-  this->_loadModules();
-  this->_loadWorkforce();
+    this->_loadModules();
+    this->_loadWorkforce();
 }
