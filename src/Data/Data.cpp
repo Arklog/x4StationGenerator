973a59ecb6fda55d7783ffb7fb608da93df3327e
Arklog <pierr.ducloux@gmail.com>
2025-10-19 14:36:41 +0200

    adding new class, push for saving

---
+++ src/Data/Data.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pierre on 10/19/25.
+//
+
+#include "Data.hpp"
---
+++ src/Data/Data.hpp
@@ -0,0 +1,18 @@
+//
+// Created by pierre on 10/19/25.
+//
+
+#ifndef X4STATIONGENERATOR_DATA_HPP
+#define X4STATIONGENERATOR_DATA_HPP
+#include "RawData.hpp"
+
+struct ModuleData
+{
+  std::vector<Module> modules;
+};
+
+class Data
+{
+};
+
+#endif // X4STATIONGENERATOR_DATA_HPP
