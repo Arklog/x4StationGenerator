e79561ac794f249789e94354bcc39f19ad67ba44
Pierre <pierr.ducloux@gmail.com>
2026-07-08 01:34:04 +0200

    added wares translation support

--- src/libextractor/extractor/models/Wares.cpp
+++ src/libextractor/extractor/models/Wares.cpp
@@ -4,5 +4,21 @@
 
 #include "Wares.hpp"
 
+#include "extractor/LangFile.hpp"
+
 namespace extractor::models::wares {
 } // extractor
+
+void extractor::models::translate(const extractor::LangFile &lang_file, Wares &wares) {
+    for (auto &production_method: wares.production.method) {
+        lang_file.translate(production_method.name.value().value());
+    }
+
+    for (auto &ware: wares.ware) {
+        lang_file.translate(ware.name.value());
+
+        for (auto &production: ware.production) {
+            lang_file.translate(production.name.value().value());
+        }
+    }
+}
