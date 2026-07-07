0b9741aee85638cd7e66e2f968133420395681ad
Pierre <pierr.ducloux@gmail.com>
2026-07-08 01:10:25 +0200

    Added translation file parsing

---
+++ src/libextractor/extractor/models/T.hpp
@@ -0,0 +1,23 @@
+//
+// Created by pierre on 7/8/26.
+//
+
+#ifndef X4STATIONGENERATOR__T_HPP
+#define X4STATIONGENERATOR__T_HPP
+#include <rfl/cli.hpp>
+#include <rfl/xml.hpp>
+
+namespace extractor::models {
+    struct T {
+        struct Page {
+            struct T {
+                rfl::Attribute<std::string> id;
+                std::string                 xml_content;
+            };
+
+            rfl::Attribute<std::string> id;
+        };
+    };
+} // extractor
+
+#endif //X4STATIONGENERATOR__T_HPP
