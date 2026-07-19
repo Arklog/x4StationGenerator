f9dab070aaeb21a85dda0c571caf73e263486d30
Pierre <pierr.ducloux@gmail.com>
2026-07-19 17:38:13 +0200

    updated generator for data rework

--- src/libcommon/common/types/Workforce.cpp
+++ src/libcommon/common/types/Workforce.cpp
@@ -4,7 +4,18 @@
 
 #include "Workforce.hpp"
 
-namespace common {
-    namespace types {
-    } // types
-}     // common
+#include <algorithm>
+#include <cmath>
+
+namespace common::types {
+    Workforce::workforce_consumption Workforce::getConsumption(unsigned int amount) const {
+        workforce_consumption consumption{};
+
+        std::ranges::for_each(this->consumption, [&consumption, amount](auto consumed_ware) {
+            consumed_ware.second = std::ceil(consumed_ware.second * amount);
+            consumption.emplace(consumed_ware.first, consumed_ware.second);
+        });
+
+        return consumption;
+    }
+}
